20ce2dac1b669ea3f9a3ffed1e71d4d4cc219724
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-08 13:56:49 +0530

    card2😊

--- react-card-component/src/App.jsx
+++ react-card-component/src/App.jsx
@@ -1,12 +1,13 @@
 import Card from './Card.jsx'
+import Card2 from './Card2.jsx'
 
 function App() {
   return(
     <>
     <Card/>
+    <Card2/>
     <Card/>
-    <Card/>
-    <Card/>
+    <Card2/>
     </>
   );
 }
---
+++ react-card-component/src/Card2.jsx
@@ -0,0 +1,24 @@
+import profilePic from './assets/kennew1.jpg'
+
+function Card2() {
+    return(
+        <div className="card2" >
+            <img className="card2-image" src= {profilePic} alt="profilePic"></img>
+            <h1 className="card2-title"> ken </h1>
+            <p className="card2-text"> I'm master of street figthers💪😤👊 </p>
+        </div>
+    );
+}
+
+export default Card2
+
+
+
+
+
+
+
+
+
+
+
